2df23c337d1480bd276e7f726a30aa6cfac9b106
Icyblue <guan8175@gmail.com>
2022-11-19 23:46:25 +0300

    restart button works

--- script.js
+++ script.js
@@ -14,17 +14,25 @@ const cellElements = document.querySelectorAll('[data-cell]');
 const board = document.getElementById('board')
 const winningMessageElement = document.getElementById('winningMessage')
 const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+const restartButton = document.getElementById('restartButton');
+
 let circleTurn 
 
+startGame();
 
+restartButton.addEventListener('click', startGame)
 
-const startGame = (() => {
+function startGame() {
     circleTurn = false
     cellElements.forEach(cell => {
+        cell.classList.remove(X_CLASS)
+        cell.classList.remove(CIRCLE_CLASS)
+        cell.removeEventListener('click', handleClick)
         cell.addEventListener('click', handleClick, { once: true})
     })
     setBoardHoverClass()
-})();
+    winningMessageElement.classList.remove('show')
+};
 
 function handleClick(e) {
     const cell = e.target
